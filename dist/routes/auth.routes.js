e0d34cf036472fe6fcb85488ddc4489c0b35f889
ningarajgani <ningarajgani143@gmail.com>
2026-09-10 13:38:23 +0530

    fix(build): compile tsc in build script and include updated dist for production

--- dist/routes/auth.routes.js
+++ dist/routes/auth.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { register, login, loginOtp, getProfile, updateProfile } from "../controllers/auth.controller.js";
+import { register, login, loginOtp, sendOtp, verifyOtp, getProfile, updateProfile, } from "../controllers/auth.controller.js";
 import { authenticate } from "../middlewares/auth.js";
 const router = Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/login-otp", loginOtp);
+router.post("/send-otp", sendOtp);
+router.post("/verify-otp", verifyOtp);
 router.get("/profile", authenticate, getProfile);
 router.put("/profile", authenticate, updateProfile);
 export default router;
